3e48ce584f95a6d155e635f6de591566e92b9f52
Abayomi Saberedowo <yomisabere@gmail.com>
2023-03-16 17:09:56 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that prints a line of code followed by a new line to the statndard error.
- * Return 1
- */
-
-int main(void)
+ * main - A c program that prints a line to the standard error
+ * Code by Yomi Sabs
+ * Return: 1
+*/
+int main (void)
 {
 	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+return (1);
 }
