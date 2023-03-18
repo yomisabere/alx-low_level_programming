4dde5d0416ecb6065e1f8149143c740f930ccd34
Abayomi Saberedowo <yomisabere@gmail.com>
2023-03-17 22:54:03 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints letters of the alphabet in lowercase
+ * Return: 0
+ *
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
